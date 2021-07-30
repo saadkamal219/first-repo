bc06d2a8d851586f557b417dd585b8dbdfe6af61
Saad Kamal <saadkamal219@gmail.com>
2021-07-30 22:20:22 +0600

    my first git push

---
+++ run.py
@@ -0,0 +1,15 @@
+class Number(object):
+    def __init__(self, num1: int, num2: int):
+        self.num1 = num1
+        self.num2 = num2
+    
+    def add(self) -> int:
+        return self.num1 + self.num2
+
+
+if __name__ == '__main__':
+    num1 = input('please enter the first number: ')
+    num2 = input('please enter the second number: ')
+
+    number = Number(int(num1), int(num2))
+    print(number.add())
