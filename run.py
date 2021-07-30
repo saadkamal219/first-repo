43be50b79067d87843bc8ce0e6ea1a7633730b63
Saad Kamal <saadkamal219@gmail.com>
2021-07-30 22:24:47 +0600

    add substract method

--- run.py
+++ run.py
@@ -6,10 +6,14 @@ class Number(object):
     def add(self) -> int:
         return self.num1 + self.num2
 
+    def substract(self) -> int:
+        return abs(self.num1 - self.num2)
+
 
 if __name__ == '__main__':
     num1 = input('please enter the first number: ')
     num2 = input('please enter the second number: ')
 
     number = Number(int(num1), int(num2))
-    print(number.add())
+    print('sum is:', number.add())
+    print('substract is:', number.substract())
